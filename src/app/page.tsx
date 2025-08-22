699ba01bab2b722040a672a852fafe6b9eb2e620
geugyeonglabs <geugyeonglabs@gmail.com>
2025-08-22 18:49:41 +0900

    design: 헤더 및 설정 버튼

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import SettingsIcon from "@mui/icons-material/Settings";
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <>
+      <header className="p-1 text-right">
+        <button>
+          <SettingsIcon />
+        </button>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
