34724ece5acdcb3e14aaa2e87babf38efe62d895
geugyeonglabs <geugyeonglabs@gmail.com>
2025-08-23 12:22:01 +0900

    design: 메인 디스플레이 및 푸터

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,37 @@ import SettingsIcon from "@mui/icons-material/Settings";
 
 export default function Home() {
   return (
-    <>
-      <header className="p-1 text-right">
+    <div className="h-dvh">
+      <header className="mb-[5dvh] p-2 text-right">
         <button>
           <SettingsIcon />
         </button>
       </header>
-      <main></main>
-      <footer></footer>
-    </>
+      <main className="flex flex-col justify-center items-center gap-16">
+        <section className="flex flex-col items-center gap-4">
+          <div className="text-5xl font-medium">23</div>
+          <div className="text-3xl">토요일</div>
+        </section>
+        <section className="flex flex-col items-center text-xl text-gray-500">
+          <div>
+            절주 <span className="text-2xl font-medium text-black">7</span>일차
+          </div>
+          <div>
+            금주 <span className="text-2xl font-medium text-black">7</span>일차
+          </div>
+        </section>
+        <section className="flex flex-col items-center gap-6">
+          <div className="text-2xl">금주 종료</div>
+          <div>
+            <span className="text-9xl text-blue-700">0일</span>
+            <span className="text-lg text-gray-500">전</span>
+          </div>
+        </section>
+      </main>
+      <footer className="absolute bottom-8 flex flex-col items-center w-dvw text-gray-400">
+        <div>금주 기간은 08.16 ~ 08.22 입니다.</div>
+        <div>음주가능 기간은 08.23 ~ 08.23 입니다.</div>
+      </footer>
+    </div>
   );
 }
